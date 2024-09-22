0517a01921c9faacb48297b0da99655922a26965
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2024-09-22 19:05:20 +0530

    MapProblems

---
+++ FrequencyOfElementsInAnArray.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyOfElementsInAnArray {
+    public static void main(String[] args) {
+        // [1,2,1,2,1,1,4,5,6]
+        // 1--> 4 2 --> 2 4-->1 5 -->1 6 -->1
+
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        Map<Integer, Integer> ans = new HashMap<Integer,Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(1);
+        nums.add(2);
+        nums.add(1);
+        nums.add(1);
+        nums.add(4);
+        nums.add(5);
+        nums.add(6);
+
+        
+        for(int num:nums)
+        {
+            if(ans.keySet().contains(num))
+            {
+                ans.put(num, ans.get(num)+1);
+            }
+            else{
+                ans.put(num, 1);
+            }
+        }
+        System.out.println(ans);
+    }
+}
